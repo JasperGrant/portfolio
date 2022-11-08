7ba2c00b65d0a774d615c337937c103fa3294dae
JasperGrant <72110751+JasperGrant@users.noreply.github.com>
2022-11-08 02:06:40 -0400

    Added about me page and additional projects

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import Jasper from '../img/Jasper.png'
+import Jasper from '../img/Jasper.png';
 
 export default function Homepage() {
     return (
@@ -9,16 +9,17 @@ export default function Homepage() {
             <br/>
             <img className=" border-neutral border-8 mx-auto rounded-full w-36 h-36" src={Jasper} alt="Headshot"/>
             <br/>
-            <div className=" rounded-lg bg-neutral text-center border-8 border-turquoise">
+            <div className=" rounded-lg bg-neutral text-center border-8 border-plum">
                 <br/>
                 <h1 className="text-3xl mb-8">Jasper Grant</h1>
                 <h2 className="text-xl mb-8">Electrical Engineering Student with Computer Option</h2>
+                <h2 className="text-xl mb-8">Software Engineering Co-op Student at ISE</h2>
             </div>
             <br/>
             <br/>
             <br/>
             <Link to="projects"
-                  className=" rounded-lg bg-neutral text-center px-3 py-2 border-8 hover:text-white text-turquoise">Click
+                  className=" rounded-lg bg-neutral text-center px-3 py-2 border-8 text-white hover:text-turquoise">Click
                 here to see
                 my projects</Link>
             <br/>
